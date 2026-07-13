9f3a38772540ca3c2e7b1cadff8ebdeacdeea473
Jeromesam10 <143810392+Jeromesam10@users.noreply.github.com>
2026-07-13 16:46:59 +0530

    Add files via upload

--- state.py
+++ state.py
@@ -1,7 +1,14 @@
 from typing import TypedDict
 
+
 class WorkflowState(TypedDict):
+
     input: str
+
     agent1_output: str
-    agent2_output: str
+
+    translated_text: str
+
+    translated_audio: str
+
     final_output: str
